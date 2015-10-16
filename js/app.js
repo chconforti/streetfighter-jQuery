a78fc3c776d2c1aa749ce2df097370ff0fd49150
chconforti <ch.conforti@gmail.com>
2015-10-16 15:42:16 -0400

    commit message

--- js/app.js
+++ js/app.js
@@ -8,14 +8,27 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		//play hadouken sound
-		//show hadouken and animate it to the right of the screen
+		$('.hadouken').finish().show()
+			.animate(
+				{'left': '1103'},
+				500,
+				function() {
+					$(this).hide();
+					$(this).css('left', '603px');
+				}
+			);
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
